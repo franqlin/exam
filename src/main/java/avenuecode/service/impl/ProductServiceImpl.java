e219edfee84901858c9b76ae70055cb791009325
Franqlin Soares Santos <franqlin.soares@gmail.com>
2016-11-29 17:49:22 -0200

    limpeza do codigo

--- src/main/java/avenuecode/service/impl/ProductServiceImpl.java
+++ src/main/java/avenuecode/service/impl/ProductServiceImpl.java
@@ -25,49 +25,38 @@ import org.springframework.stereotype.Component;
 public class ProductServiceImpl implements ProductService {
 	 @PersistenceContext 
      private EntityManager em;
-     NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     @Resource(name= "productRepository")
     private ProductRepository productRepository;
 
-    @Autowired
-    public void setNamedParameterJdbcTemplate(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    }
+
 
   @Override
   public List<Product> getAllProductIncludingRelationship() {
-    //return em.createQuery("select p from Product p", Product.class).getResultList();
     return productRepository.findAllWithRelationship();
   }
     @Override
    public List<NoParentProjection> getAllProductExcludingRelationship() {
-        //return em.createQuery("select p.id,p.description,p.name from Product p").getResultList();
         return productRepository.getAllProductExcludingRelationship();
    }
     @Override
     public Product getProductById(Long  productId) {
-        //return em.find(Product.class,productId);
         return productRepository.findById(productId);
     }
     @Override
     public List<Product> getChildProductList(Long  productId){
         return productRepository.findChildProductListById(productId);
-        //return em.createQuery("select p from Product p where p.parentProduct.id = :productId", Product.class).setParameter("productId", productId).getResultList();
     }
     @Override
     public List<Image> getImageList(Long  productId){
         return productRepository.getImageList(productId);
-        //return em.createQuery("select i from Image i where i.productId.id = :productId", Image.class).setParameter("productId", productId).getResultList();
     }
     @Override
     public NoParentProjection getProductExcludingRelationshipId(Long productId){
-        //return em.createQuery("select p.id,p.description,p.name from Product p where p.id= :productId").setParameter("productId", productId).getSingleResult();
         return productRepository.getProductExcludingRelationship(productId);
     }
     @Override
     public List<NoParentProjection>  teste(){
-
-        return productRepository.getAllProductExcludingRelationship();
+     return productRepository.getAllProductExcludingRelationship();
     }
 
 }
